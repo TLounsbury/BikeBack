33681aa39f8a0cb9b04087e684cfb36b349d7c49
Parker Dunbar <Parker.Dunbar@fmr.com>
2020-03-24 13:55:33 -0400

    Required taxes

--- Bike/routes/index.js
+++ Bike/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bikes = require('../modules/bikes');
+var taxes = require('../modules/taxes')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
